e138fa70b6f8070acb10892bec030c6795dd5c94
Fgitee <33415636+Fgitee@users.noreply.github.com>
2021-03-13 19:08:00 +0300

    Create About.js

---
+++ src/containers/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+//
+
+export default () => (
+  <div>
+    <h3>The life and times of Sean Connery</h3>
+    <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F171208101828-13-sean-connery-restricted.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />
+      <p>Connery grew up in a working-class family. After a three-year stint in the Royal Navy and a series of odd jobs, he began practicing bodybuilding and became a model for student artists and men’s fashion catalogs. </p>
+      <p>He competed in the 1953 Mr. Universe contest, which in turn led to work as an extra in stage productions. In 1954 he landed a small part in a touring production of the Rodgers and Hammerstein musical South Pacific and eventually took the leading role. More stage and television work followed, including a much-praised performance as washed-up boxer Mountain Rivera in the BBC television production of Rod Serling’s Requiem for a Heavyweight in 1957.</p>
+      <p>In 1962 Connery was cast in the role of James Bond, Agent 007 of the British Secret Intelligence Service, in the screen adaptation of Ian Fleming’s spy thriller Dr. No. The immense success of the film and its immediate sequels, From Russia with Love (1963) and Goldfinger (1964), established the James Bond films as a worldwide phenomenon and Connery as an international celebrity. Not wanting to be typecast as the superspy, Connery continued to take other acting roles, notably in Alfred Hitchcock’s psychological thriller Marnie (1964). After completing the next two James Bond films, Thunderball (1965) and You Only Live Twice (1967), Connery renounced the role of Bond. Four years later, however, he was persuaded to return to the role for Diamonds Are Forever (1971), which he declared was his last movie as Bond.</p>
+  </div>
+)
