c35a0a364ca14f066df09240bb0b7032bb24a18f
Fgitee <33415636+Fgitee@users.noreply.github.com>
2021-03-13 19:22:11 +0300

    Create Gallery.js

---
+++ src/containers/Gallery.js
@@ -0,0 +1,70 @@
+import React from 'react'
+//
+
+export default () => (
+  <div>
+    <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F171117212302-02-sean-connery-restricted.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />  
+  <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F171117212306-04-sean-connery-restricted.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />
+  <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F201031091700-01-connery-1962.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />
+  <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F171208095213-06-sean-connery-restricted.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />
+  <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F171208095216-08-sean-connery-restricted.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />
+  <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F171208095217-09-sean-connery-restricted.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />
+   <StaticImage
+      src="https://dynaimage.cdn.cnn.com/cnn/w_1600/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F171208101831-15-sean-connery-restricted.jpg"
+      alt="Sean Connery"
+      width={400}
+      height={500}
+      quality={100}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      style={{ marginBottom: `1.45rem` }}
+    />
+  </div>
+)
